4ae2eb4300683f91c4f4c55540ca8fcfa086e489
Thomas Wening <thomaswening94@gmail.com>
2024-01-17 20:11:47 +0100

    add view model for note pad with text property

---
+++ NotePadClone/NotePadViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+using Microsoft.Win32;
+
+using WpfEssentials.Base;
+
+namespace NotePadClone;
+internal class NotePadViewModel : ObservableObject
+{
+    private string? _textContent;
+
+    public string? TextContent
+    {
+        get => _textContent;
+        set
+        {
+            if (_textContent != value)
+            {
+                _textContent = value;
+                OnPropertyChanged(nameof(TextContent));
+            }
+        }
+    }
+}
