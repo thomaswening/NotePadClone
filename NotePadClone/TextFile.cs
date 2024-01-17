6e08fd57786b7f0462903ad1f778f5bd0dda2e71
Thomas Wening <thomaswening94@gmail.com>
2024-01-17 20:06:08 +0100

    add model for text file

---
+++ NotePadClone/TextFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotePadClone;
+internal class TextFile
+{
+    public string? Content { get; set; }
+    public string? FilePath { get; set; }
+}
