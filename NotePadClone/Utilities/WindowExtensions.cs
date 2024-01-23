2b678fe4a042c200c47158a94513baace61dd287
Thomas Wening <thomaswening94@gmail.com>
2024-01-23 21:35:07 +0100

    add extension for the window class to implement what the window chrome buttons do

---
+++ NotePadClone/Utilities/WindowExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NotePadClone.Utilities;
+
+/// <summary>
+/// Extension methods for the <see cref="Window"/> class.
+/// </summary>
+public static class WindowExtensions
+{
+    public static void Minimize(this Window window)
+    {
+        window.WindowState = WindowState.Minimized;
+    }
+
+    public static void Maximize(this Window window)
+    {
+        window.WindowState = WindowState.Maximized;
+    }
+
+    public static void Restore(this Window window)
+    {
+        window.WindowState = WindowState.Normal;
+    }
+}
