e44ae94367129e990f4ec80b951cd4021d4915ac
Thomas Wening <thomaswening94@gmail.com>
2024-01-25 09:05:44 +0100

    implement service for window operations

---
+++ NotePadClone/Services/IWindowService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotePadClone.Services
+{
+    public interface IWindowService
+    {
+        void OpenNewWindow();
+    }
+}
---
+++ NotePadClone/Services/WindowService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotePadClone.Services
+{
+    internal class WindowService : IWindowService
+    {
+        public void OpenNewWindow() 
+        {
+            var window = new MainWindow();
+            window.Show();
+        }
+    }
+}
