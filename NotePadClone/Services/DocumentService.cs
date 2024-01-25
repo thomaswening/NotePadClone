79d39d8e27dd7b3c4cea3d34568a7ece2f9817af
Thomas Wening <thomaswening94@gmail.com>
2024-01-25 21:55:49 +0100

    implement service to handle opening and saving of documents

---
+++ NotePadClone/Services/DocumentService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NotePadClone.Models;
+
+namespace NotePadClone.Services;
+public class DocumentService : IDocumentService
+{
+    public IDocument? OpenDocument(Func<string?>? fileSelectionHandler)
+    {
+        var filePath = fileSelectionHandler?.Invoke();
+        if (string.IsNullOrEmpty(filePath))
+            return null;
+
+        var content = File.ReadAllText(filePath);
+        if (content is null)
+            return null;
+
+        return new Document(filePath, content);
+    }
+
+    public void SaveAs(Func<string?>? fileSelectionHandler, IDocument document)
+    {
+        var filePath = fileSelectionHandler?.Invoke();
+        if (string.IsNullOrEmpty(filePath))
+            return;
+
+        document.FilePath = filePath;
+        File.WriteAllText(filePath, document.Content);
+    }
+
+    public void Save(IDocument document)
+    {
+        if (string.IsNullOrEmpty(document.FilePath))
+            return;
+
+        File.WriteAllText(document.FilePath, document.Content);
+    }
+}
---
+++ NotePadClone/Services/IDocumentService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NotePadClone.Models;
+
+namespace NotePadClone.Services;
+
+/// <summary>
+/// Service for opening and saving documents.
+/// </summary>
+public interface IDocumentService
+{
+    ///<summary>
+    /// Opens a file and returns its content.
+    /// </summary>
+    /// <param name="fileSelectionHandler">Delegate to open the file selection dialog.</param>
+    /// <returns>The content of the file.</returns>
+    IDocument? OpenDocument(Func<string?>? fileSelectionHandler);
+
+    /// <summary>
+    /// Saves a document to a file.
+    /// </summary>
+    /// <param name="fileSelectionHandler">Delegate to open the save file dialog.</param>
+    /// <param name="document">Document to save to the chosen path.</param>
+    void SaveAs(Func<string?>? fileSelectionHandler, IDocument document);
+
+    /// <summary>
+    /// Saves a document to its filepath.
+    /// </summary>
+    /// <param name="document">Document to save.</param>
+    void Save(IDocument document);
+}
