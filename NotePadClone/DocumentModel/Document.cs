414eb440979e00f71b7f672b971d364453733c6c
Thomas Wening <thomaswening94@gmail.com>
2024-01-25 22:38:51 +0100

    refactor document metadata logic out of document class and into document metadata class

--- NotePadClone/DocumentModel/Document.cs
+++ NotePadClone/DocumentModel/Document.cs
@@ -29,48 +29,7 @@ public class Document : ObservableObject, IDocument
             if (!SetField(ref _content, value))
                 return;
 
-            UpdateMetadata();
+            Metadata.Update(Content);
         }
-    }
-
-    private void UpdateMetadata()
-    {
-        Metadata.NumberOfCharacters = GetNumberOfVisibleCharacters();
-        Metadata.NumberOfLines = GetNumberOfLines();
-        Metadata.FileSizeInBytes = GetFileSizeInBytes();
-    }
-
-    private int GetNumberOfVisibleCharacters()
-    {
-        if (string.IsNullOrEmpty(Content))
-        {
-            return 0;
-        }
-
-        const string visibleCharactersRegexStr = @"[^\p{Cc}^\p{Cn}^\p{Cs}]";
-        return Regex.Matches(Content, visibleCharactersRegexStr).Count;
-    }
-
-    private int NumberOfNewLines => Regex.Matches(Content ?? string.Empty, Environment.NewLine).Count;
-
-    private int GetNumberOfLines()
-    {
-        if (string.IsNullOrEmpty(Content)
-            || (NumberOfNewLines == 0 && Metadata.NumberOfCharacters == 0))
-        {
-            return 0;
-        }
-
-        return NumberOfNewLines + 1;
-    }
-
-    private int GetFileSizeInBytes()
-    {
-        if (string.IsNullOrEmpty(Metadata.FilePath))
-        {
-            return 0;
-        }
-
-        return (int)Convert.ToInt64(new FileInfo(Metadata.FilePath).Length);
     }
 }
--- NotePadClone/DocumentModel/DocumentMetadata.cs
+++ NotePadClone/DocumentModel/DocumentMetadata.cs
@@ -1,14 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
+using WpfEssentials.Base;
+
 namespace NotePadClone.DocumentModel;
-public class DocumentMetadata
+public class DocumentMetadata : ObservableObject
 {
-    public string? FilePath { get; set; }
-    public int NumberOfCharacters { get; set; }
-    public int NumberOfLines { get; set; }
-    public int FileSizeInBytes { get; set; }
+    public string? _filePath;
+    public int _numberOfCharacters;
+    public int _numberOfLines;
+    public int _fileSizeInBytes;
+
+    public string? FilePath
+    {
+        get => _filePath;
+        set => SetField(ref _filePath, value);
+    }
+
+    public int NumberOfCharacters
+    {
+        get => _numberOfCharacters;
+        set => SetField(ref _numberOfCharacters, value);
+    }
+    public int NumberOfLines
+    {
+        get => _numberOfLines;
+        set => SetField(ref _numberOfLines, value);
+    }
+    public int FileSizeInBytes
+    {
+        get => _fileSizeInBytes;
+        set => SetField(ref _fileSizeInBytes, value);
+    }
+
+    public void Update(string documentContent)
+    {
+        NumberOfCharacters = GetNumberOfVisibleCharacters(documentContent);
+        NumberOfLines = GetNumberOfLines(documentContent);
+        FileSizeInBytes = GetFileSizeInBytes();
+    }
+
+    private static int GetNumberOfVisibleCharacters(string documentContent)
+    {
+        if (string.IsNullOrEmpty(documentContent))
+        {
+            return 0;
+        }
+
+        const string visibleCharactersRegexStr = @"[^\p{Cc}^\p{Cn}^\p{Cs}]";
+        return Regex.Matches(documentContent, visibleCharactersRegexStr).Count;
+    }
+
+    private static int GetNumberOfNewLines(string documentContent) => Regex.Matches(documentContent ?? string.Empty, Environment.NewLine).Count;
+
+    private int GetNumberOfLines(string documentContent)
+    {
+        var numberOfNewLines = GetNumberOfNewLines(documentContent);
+
+        if (string.IsNullOrEmpty(documentContent)
+            || (numberOfNewLines == 0 && NumberOfCharacters == 0))
+        {
+            return 0;
+        }
+
+        return numberOfNewLines + 1;
+    }
+
+    private int GetFileSizeInBytes()
+    {
+        if (string.IsNullOrEmpty(FilePath))
+        {
+            return 0;
+        }
+
+        return (int)Convert.ToInt64(new FileInfo(FilePath).Length);
+    }
 }
