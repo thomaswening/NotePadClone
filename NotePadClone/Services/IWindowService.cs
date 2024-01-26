e0b0c5223942cd40f51d55b6fe588f871be0779d
Thomas Wening <thomaswening94@gmail.com>
2024-01-26 10:00:01 +0100

    refactor logic from WindowExtensions to WindowService

--- NotePadClone/Services/IWindowService.cs
+++ NotePadClone/Services/IWindowService.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+
+using NotePadClone.ViewModels;
 
 namespace NotePadClone.Services
 {
@@ -15,5 +18,37 @@ namespace NotePadClone.Services
         /// Opens a new window.
         /// </summary>
         void OpenNewWindow();
+
+        /// <summary>
+        /// Minimizes the window.
+        /// </summary>
+        /// <param name="window">Window to minimize.</param>
+        void Minimize(Window window);
+
+        /// <summary>
+        /// Maximizes the window.
+        /// </summary>
+        /// <param name="window">Window to maximize.</param>
+        void Maximize(Window window);
+
+        /// <summary>
+        /// Restores the window.
+        /// </summary>
+        /// <param name="window">Window to restore.</param>
+        void Restore(Window window);
+
+        /// <summary>
+        /// Subscribes to the view model's window operation events, i.e. close, minimize, maximize, and restore.
+        /// </summary>
+        /// <param name="window">Window to subscribe to the events.</param>
+        /// <param name="viewModel">View model that exposes the events to subscribe to.</param>
+        void SubscribeToWindowEvents(Window window);
+
+        /// <summary>
+        /// Switches the window theme from dark to light and vice versa.
+        /// </summary>
+        /// <param name="window">Window whose theme to switch.</param>
+        /// <param name="viewModel">Window's view model.</param>
+        void SwitchWindowTheme(Window window);
     }
 }
--- NotePadClone/Services/WindowService.cs
+++ NotePadClone/Services/WindowService.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+
+using NotePadClone.ViewModels;
 
 namespace NotePadClone.Services
 {
@@ -12,6 +15,49 @@ namespace NotePadClone.Services
         {
             var window = new MainWindow();
             window.Show();
+        }
+
+        private const string DarkThemeUri = "pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Dark.xaml";
+        private const string LightThemeUri = "pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Light.xaml";
+
+        public void Minimize(Window window)
+        {
+            window.WindowState = WindowState.Minimized;
+        }
+
+        public void Maximize(Window window)
+        {
+            window.WindowState = WindowState.Maximized;
+        }
+
+        public void Restore(Window window)
+        {
+            window.WindowState = WindowState.Normal;
+        }
+
+        public void SubscribeToWindowEvents(Window window)
+        {
+            var viewModel = (WindowVm)window.DataContext;
+            viewModel.CloseWindowRequestedEvent += (_, _) => window.Close();
+            viewModel.MinimizeWindowRequestedEvent += (_, _) => Minimize(window);
+            viewModel.MaximizeWindowRequestedEvent += (_, _) => Maximize(window);
+            viewModel.RestoreWindowRequestedEvent += (_, _) => Restore(window);
+        }
+
+        public void SwitchWindowTheme(Window window)
+        {
+            var viewModel = (WindowVm)window.DataContext;
+            var resourceDictionary = GetThemeResourceDictionary(viewModel.IsDarkTheme);
+            window.Resources.MergedDictionaries.Clear();
+            window.Resources.MergedDictionaries.Add(resourceDictionary);
+        }
+
+        private static ResourceDictionary GetThemeResourceDictionary(bool isDarkTheme)
+        {
+            var resourceDictionary = new ResourceDictionary();
+            string themeResource = isDarkTheme ? DarkThemeUri : LightThemeUri;
+            resourceDictionary.Source = new Uri(themeResource, UriKind.Absolute);
+            return resourceDictionary;
         }
     }
 }
