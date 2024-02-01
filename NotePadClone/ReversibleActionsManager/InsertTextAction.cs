0da0c7bb4496c0a3b41d5a2dacb0fd80e0249d54
Thomas Wening <thomaswening94@gmail.com>
2024-02-01 13:41:47 +0100

    implement reversible action to insert text into document

---
+++ NotePadClone/ReversibleActionsManager/InsertTextAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NotePadClone.DocumentModel;
+
+namespace NotePadClone.ReversibleActions;
+public class InsertTextAction(IDocument document, string text, int position) : IReversibleAction
+{
+    private readonly IDocument _document = document;
+    private readonly string _text = text;
+    private readonly int _position = position;
+
+    public void Execute()
+    {
+        _document.Insert(_position, _text);
+    }
+
+    public void Undo()
+    {
+        _document.Delete(_position, _text.Length);
+    }
+}
