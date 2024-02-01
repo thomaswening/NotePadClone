2cab7681705a62244007950966eb2c1a9dea9ee0
Thomas Wening <thomaswening94@gmail.com>
2024-02-01 13:40:15 +0100

    add interface for reversible actions

---
+++ NotePadClone/ReversibleActionsManager/IReversibleAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotePadClone.ReversibleActions;
+
+/// <summary>
+/// Represents an action that can be historized for undo/redo.
+/// </summary>
+public interface IReversibleAction
+{   
+    void Execute();
+    void Undo();
+}
