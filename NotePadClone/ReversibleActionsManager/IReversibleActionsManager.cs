2c46561fde955d6483d8dde56b0cb9ad8025400a
Thomas Wening <thomaswening94@gmail.com>
2024-02-01 13:40:53 +0100

    add service to manage reversible actions

---
+++ NotePadClone/ReversibleActionsManager/IReversibleActionsManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotePadClone.ReversibleActions;
+
+/// <summary>
+/// Historizes actions for and manages their undo/redo execution.
+/// </summary>
+public interface IReversibleActionsManager
+{
+    /// <summary>
+    /// Executes an action and historizes it for undo/redo.
+    /// </summary>
+    /// <param name="action">Action to be historized.</param>
+    void ExecuteAction(IReversibleAction action);
+
+    /// <summary>
+    /// Undoes the last action.
+    /// </summary>
+    void UndoLastAction();
+
+    /// <summary>
+    /// Redoes the last action.
+    /// </summary>
+    void RedoLastAction();
+}
---
+++ NotePadClone/ReversibleActionsManager/ReversibleActionsManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotePadClone.ReversibleActions;
+public class ReversibleActionsManager : IReversibleActionsManager
+{
+    private readonly Stack<IReversibleAction> _undoStack = new();
+    private readonly Stack<IReversibleAction> _redoStack = new();
+
+    public void ExecuteAction(IReversibleAction action)
+    {
+        action.Execute();
+        _undoStack.Push(action);
+        _redoStack.Clear();
+    }
+
+    public void UndoLastAction()
+    {
+        if (_undoStack.Count == 0)
+        {
+            return;
+        }
+
+        var action = _undoStack.Pop();
+        action.Undo();
+        _redoStack.Push(action);
+    }
+
+    public void RedoLastAction()
+    {
+        if (_redoStack.Count == 0)
+        {
+            return;
+        }
+
+        var action = _redoStack.Pop();
+        action.Execute();
+        _undoStack.Push(action);
+    }
+}
